416a45c70e3797e3f50bd67f0e1ac34ca6237032
MinhThang <cdgamer0123@gmail.com>
2024-08-08 21:27:28 +0700

    feat(homepage): header, connect account card and button component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Header } from "./layouts/header/index.tsx";
+import { Footer } from "./layouts/footer/index.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <Header />
     <App />
-  </React.StrictMode>,
-)
+    <Footer />
+  </BrowserRouter>
+);
