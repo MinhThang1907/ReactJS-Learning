0f23404935cb1c612dcc66ee6418499c86dfa1fc
MinhThang <cdgamer0123@gmail.com>
2024-08-09 23:31:54 +0700

    learning useEffect hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import routes from "./routes";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [width, setWidth] = useState<number>(window.innerWidth);
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+      console.log(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <Routes>
       {routes.map((route) => (
